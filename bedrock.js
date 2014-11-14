ddfef52e13597536a5824842e00b33fff84826d6
James Hartig <james.hartig@grooveshark.com>
2014-11-14 01:13:42 -0500

    Check that model exists before trying to verify uniqueness

--- bedrock.js
+++ bedrock.js
@@ -637,6 +637,7 @@
 
         // Do not add multiple models with the same `id`.
         model = existing || model;
+        if (!model) continue;
         if (order && !modelMap[model.id]) {
           order.push(model);
 
